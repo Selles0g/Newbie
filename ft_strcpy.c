b4ccb518dd00ff36846a2765e846ccc7222d7739
SellesOg <adrisega311203@gmail.com>
2025-08-27 00:20:11 +0200

    ft_strcpy

---
+++ ft_strcpy.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <stdio.h>
+
+char    *ft_strcpy(char *dest, char *src)
+{
+    int i;
+    i = 0;
+    while(src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return(dest);
+}
+
+int main(void)
+{
+    char src[] = "Hola";
+    char dest[] = "mundo";
+
+    ft_strcpy(dest, src);
+    printf("%s", dest);
+}
